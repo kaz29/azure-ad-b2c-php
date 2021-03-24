d0d2a71772b376cc296a470176c512fb13a33aac
Kaz Watanabe <cyo@mac.com>
2021-03-25 04:45:16 +0900

    add logic to set header and claims.

--- src/Entity/AuthorizationCallbackParams.php
+++ src/Entity/AuthorizationCallbackParams.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace kaz29\AzureADB2C\Entity;
 
+use JOSE_JWS;
+
 /**
  * AuthorizationCallbackParams
  *
@@ -19,4 +21,30 @@ class AuthorizationCallbackParams extends BaseEntity
             'id_token' => 'idToken',
         ],
     ];
+    protected $jws;
+    protected $header;
+    protected $claims;
+
+    public function setJWS(JOSE_JWS $jws): void
+    {
+        $this->jws = $jws;
+
+        $this->header = new Header((array)$jws->header);
+        $this->claims = new Claims((array)$jws->claims);
+    }
+
+    public function getJWS(): JOSE_JWS
+    {
+        return $this->jws;
+    }
+
+    public function getClaims(): Claims
+    {
+        return $this->claims;
+    }
+
+    public function getHeader(): Header
+    {
+        return $this->header;
+    }
 }
